6eb242e150ec73439187d5dd53c89e5b000fb61d
Tiago Comiotto <tiago.comiotto@gps.com.br>
2026-07-25 20:06:13 -0300

    feat: botao abrir pedido com selecao multipla no estoque

---
+++ busca_status_form.py
@@ -0,0 +1,5 @@
+﻿lines = open('static/index.html', 'r', encoding='utf-8').readlines()
+# Procurar hidden fields ou inputs de status no formulário
+for i in range(1095, 1200):
+    if 'status' in lines[i].lower() or 'hidden' in lines[i].lower():
+        print(f'{i+1}: {lines[i]}', end='')
---
+++ busca_status_pedido.py
@@ -0,0 +1,7 @@
+﻿lines = open('static/index.html', 'r', encoding='utf-8').readlines()
+# Procurar status_pedido no HTML
+for i in range(len(lines)):
+    if 'status_pedido' in lines[i]:
+        print(f'{i+1}: {lines[i]}', end='')
+if not any('status_pedido' in l for l in lines):
+    print('status_pedido NAO ENCONTRADO no HTML')
