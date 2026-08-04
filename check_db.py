991ec027389c7bf10eff88786fd292b999e19429
Tiago Comiotto <tiago.comiotto@gps.com.br>
2026-08-03 22:05:27 -0300

    Limpa repositório: remove artefatos de desenvolvimento e amplia .gitignore

--- check_db.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('almoxarifado.db')
-c = conn.execute("SELECT name FROM sqlite_master WHERE type='table'")
-for r in c.fetchall():
-    print(r[0])
-conn.close()
